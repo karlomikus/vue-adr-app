deba40acfa9f6ee15570b888333c480e97838550
karlomikus <contact@karlomikus.com>
2016-06-24 12:02:04 +0000

    Added saving token on login

--- src/app.js
+++ src/app.js
@@ -1,17 +1,28 @@
-let Vue = require('vue');
-let VueResource = require('vue-resource');
-let VueRouter = require('vue-router');
+import Vue from 'Vue';
+import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+import Login from './pages/Login.vue';
+import Categories from './pages/Categories.vue';
 
 // Vue Plugins
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
-// Components
-import Login from './pages/Login.vue';
-import Categories from './pages/Categories.vue';
-
 // Initiate router
-let App = Vue.extend({});
+let App = Vue.extend({
+    events: {
+        signedIn(token) {
+            console.log('User logged in!');
+            localStorage.setItem('token', token);
+        }
+    },
+    data() {
+        return {
+            'token': null,
+            'authenticated': false
+        };
+    }
+});
 let router = new VueRouter();
 
 // Map routes
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,21 +1,33 @@
 <template>
-    <section id="login">
-        <form action="/" method="post">
-          <input type="text" name="email" placeholder="Type your email">
-          <br>
-          <input type="password" name="password" placeholder="Type your password">
-          <br>
-          <button type="submit">{{ msg }}</button>
-        </form>
-      </section>
+  <form class="login" @submit.prevent="signIn()">
+    <header class="login__header">Login</header>
+    <section class="login__body">
+      <label>Email</label>
+      <input class="form-element" type="email" name="email" placeholder="Type your email" v-model="email">
+      <label>Password</label>
+      <input class="form-element" type="password" name="password" placeholder="Type your password" v-model="password">
+      <button class="btn btn--confirm" type="submit">Sign In</button>
+    </section>
+  </form>
 </template>
 
 <script>
     module.exports = {
-        data: function() {
-            return {
-                msg: 'Test'
-            }
+      data() {
+        return {
+          email: "",
+          password: ""
+        };
+      },
+      methods: {
+        signIn() {
+          let payload = { email: this.email, pass: this.password };
+          this.$http.post('http://adr.dev/login', payload).then((resp) => {
+            this.$dispatch('signedIn', resp.data.token);
+          }, (resp) => {
+            console.log('Error occured');
+          });
         }
+      }
     }
 </script>
